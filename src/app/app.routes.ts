cf1640c832a4729e9647d88b6d229179c08816b9
keytiiii <ktwarmey@gmail.com>
2024-04-12 15:59:14 +0800

    Updated Login/Register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'student-dashboard', component: StudentDashboardComponent },
     { path: 'admin-dashboard', component: AdminDashboardComponent},
     { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent},
 ];
