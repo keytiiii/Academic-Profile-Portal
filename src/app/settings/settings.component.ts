f06f3438a8e7c728fa9fd66e662ffacc248aa961
Ynnyannah <ynnyannah02@gmail.com>
2024-04-26 17:04:38 +0800

    fix

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 0e1c64f9a80c5c4c14003375d77428d62d6fcc9d
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
-<<<<<<< HEAD
-  standalone: true,
-  imports: [],
-  templateUrl: './settings.component.html',
-  styleUrl: './settings.component.css'
-})
-export class SettingsComponent {
-
-}
-=======
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
   
 }
-
->>>>>>> 0e1c64f9a80c5c4c14003375d77428d62d6fcc9d
