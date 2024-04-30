e9efe99d487c34ddb0874545edf9024c0b021885
Joemarc Pagdilao <joemarcpagdilao@gmail.com>
2024-04-30 19:52:09 +0800

    midbar and student dashboard

---
+++ src/app/midbar/midbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-midbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './midbar.component.html',
+  styleUrl: './midbar.component.css'
+})
+export class MidbarComponent {
+
+}
