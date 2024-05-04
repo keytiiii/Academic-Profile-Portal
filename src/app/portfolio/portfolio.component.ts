b253826315b7c365b6810cf0dc36ad69ff501b3d
Eiji878 <ainjell.mendigorin1@gmail.com>
2024-05-04 10:53:24 +0800

    add modal

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { StudentDashboardComponent } from '../student-dashboard/student-dashboard.component';
+import { ModalContentComponent } from '../modal-content/modal-content.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [StudentDashboardComponent],
+  imports: [ModalContentComponent, MatDialogModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+  title = 'angular-dialog';
+
+  constructor(private matDialog: MatDialog){
+
+  }
+
+  openDialog(){
+    this.matDialog.open(ModalContentComponent,{
+      width: '3500px',
+    })
+  }
 
 }
