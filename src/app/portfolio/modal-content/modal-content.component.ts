b5f95a921a658a4c01540b4374e97d0ef619dcf9
rekasa123 <regeecasana2002@gmail.com>
2024-05-04 23:42:15 +0800

    Portfolio Layout

---
+++ src/app/portfolio/modal-content/modal-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-content.component.html',
+  styleUrl: './modal-content.component.css'
+})
+export class ModalContentComponent {
+
+}
