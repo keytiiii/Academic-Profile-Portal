48075a250744cf0b8850e45da40666e761c91599
Ynnyannah <ynnyannah02@gmail.com>
2024-05-08 20:31:11 +0800

    fix midbar

--- src/app/midbar/midbar.component.spec.ts
+++ src/app/midbar/midbar.component.spec.ts
@@ -8,7 +8,7 @@ describe('MidbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MidbarComponent]
+      declarations: [MidbarComponent]
     })
     .compileComponents();
     
