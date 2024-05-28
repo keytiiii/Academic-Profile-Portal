46ba7e1782790ea4ef3c635b9f7f6abbe405aa27
Ynnyannah <ynnyannah02@gmail.com>
2024-05-28 16:19:42 +0800

    added feedback + fixed components, angular ,routes, etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FeedbackComponent } from "./feedback/feedback.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [RouterOutlet, FeedbackComponent]
 })
 export class AppComponent {
   title = 'warmey-app';
