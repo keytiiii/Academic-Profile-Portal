fce12b14f28af41e1200e97640c9f7e67828c792
Ynnyannah <ynnyannah02@gmail.com>
2024-05-28 16:42:03 +0800

    modified routes & feedback typescript

--- src/app/feedback/feedback.component.spec.ts
+++ src/app/feedback/feedback.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FeedbackComponent } from './feedback.component';
 
 describe('FeedbackComponent', () => {
@@ -8,7 +7,7 @@ describe('FeedbackComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FeedbackComponent]
+      declarations: [FeedbackComponent] 
     })
     .compileComponents();
     
